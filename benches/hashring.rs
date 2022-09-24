2951dd17758f2bc2fbac82ba9309cccaa5530369
Alex Jesipow <alexander.jesipow@gmail.com>
2022-09-24 13:22:33 +0200

    Add hashring benching

---
+++ benches/hashring.rs
@@ -0,0 +1,20 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use hulahoop::HashRing;
+use std::num::NonZeroU64;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut ring: HashRing<&str> = HashRing::new();
+    ring.add("127.0.0.1:12345", NonZeroU64::new(1).unwrap());
+    ring.add("127.0.0.1:12346", NonZeroU64::new(1).unwrap());
+    let mut group = c.benchmark_group("Getting a node for a key from the HashRing");
+    for size in ["abc", "1234", "Some very very long text"].iter() {
+        group.throughput(Throughput::Bytes(size.as_bytes().len() as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            b.iter(|| ring.get_by_key(size));
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
