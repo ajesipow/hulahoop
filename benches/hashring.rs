f47ebe3d87200006593997b316e3361ea5ba8192
Alex Jesipow <alexander.jesipow@gmail.com>
2022-10-01 08:40:36 +0200

    Bench update

--- benches/hashring.rs
+++ benches/hashring.rs
@@ -3,17 +3,43 @@ use hulahoop::HashRing;
 use std::num::NonZeroU64;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let mut ring: HashRing<&str> = HashRing::new();
-    ring.add("127.0.0.1:12345", NonZeroU64::new(100).unwrap());
-    ring.add("127.0.0.1:12346", NonZeroU64::new(100).unwrap());
-    let mut group = c.benchmark_group("Getting a node for a key from the HashRing");
-    for size in ["abc", "1234", "Some very very long text"].iter() {
-        group.throughput(Throughput::Bytes(size.as_bytes().len() as u64));
-        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
-            b.iter(|| ring.get_by_key(size));
-        });
+    {
+        let mut ring: HashRing<&str> = HashRing::new();
+        ring.add("127.0.0.1:12345", NonZeroU64::new(1).unwrap());
+        ring.add("127.0.0.1:12346", NonZeroU64::new(1).unwrap());
+        let mut group = c.benchmark_group("Getting a node for a key from the HashRing");
+        for size in ["abc", "1234", "Some very very long text"].iter() {
+            group.throughput(Throughput::Bytes(size.as_bytes().len() as u64));
+            group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+                b.iter(|| ring.get_by_key(size));
+            });
+        }
+        group.finish();
     }
-    group.finish();
+
+    {
+        let mut ring: HashRing<&str> = HashRing::new();
+        let mut group = c.benchmark_group("Adding virtual nodes");
+        for size in [1, 10, 100, 1000].iter() {
+            group.throughput(Throughput::Bytes(*size as u64));
+            group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+                b.iter(|| ring.add("127.0.0.1:12346", NonZeroU64::new(size).unwrap()));
+            });
+        }
+        group.finish();
+    }
+
+    {
+        let mut ring: HashRing<&str> = HashRing::new();
+        let mut group = c.benchmark_group("Removing virtual nodes");
+        for size in [1, 10, 100, 1000].iter() {
+            ring.add("127.0.0.1:12346", NonZeroU64::new(*size).unwrap());
+            group.bench_function(BenchmarkId::from_parameter(size), |b| {
+                b.iter(|| ring.remove("127.0.0.1:12346"))
+            });
+        }
+        group.finish();
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
